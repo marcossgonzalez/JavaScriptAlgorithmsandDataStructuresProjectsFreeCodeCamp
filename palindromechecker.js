b6d3a4ba0bb0aedb4dfe539ac740f56a2c1cadfc
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-11-05 02:29:35 -0300

    Update lesson
    ongoing lesson

--- palindromechecker.js
+++ palindromechecker.js
@@ -1,6 +1,12 @@
 //----------------------------------
 function palindrome(str) {
+    let string = str;
+    console.log(string);
+    let regex = /[^\W][a-zA-Z]*/gi;
+    string = string.match(regex).join('').replace('_','');
+    for (let i = 0)
+    console.log(string);
     return true;
 }
 
-palindrome("eye");
+palindrome("0_0 (: /-\ :) 0-0")
