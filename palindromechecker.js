e275d8e8e2b4744d27c473fa09337120124d5be7
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-11-05 03:25:38 -0300

    Update challenge finished
    update challenge finished

--- palindromechecker.js
+++ palindromechecker.js
@@ -1,31 +1,35 @@
 //----------------------------------
 function palindrome(str) {
     let string = str;
-    console.log(string);
     let regex = /[^\W][a-zA-Z]*/gi;
-    string = string.match(regex).join('').replace('_','').toLowerCase();
-    let stringLengthEven = (string.length/2) % 2;
+    string = string.match(regex).join('').replace('_', '').toLowerCase();
+    let stringLengthEven = (string.length / 2) % 2;
     let stringLength = string.length;
-    console.log(stringLength);
-    if(stringLength % 2 != 0){
-        let letterDoubleIndex = Math.floor(stringLength/2);
+    if (stringLength % 2 != 0) {
+        let letterDoubleIndex = Math.floor(stringLength / 2);
         string = Array.from(string);
-        console.log(string);
         string.splice(letterDoubleIndex, 0, string[letterDoubleIndex]);
-        console.log(string);
     }
     string = Array.from(string);
-    stringLength = Math.ceil(string.length/2);
-    console.log(stringLength)
+    stringLength = Math.ceil(string.length / 2);
     let firstHalf = string.splice(0, stringLength);
     let secondHalf = (string.splice(-stringLength)).reverse();
-    console.log(firstHalf)
-    console.log(secondHalf)
-    if(JSON.stringify(firstHalf) === JSON.stringify(secondHalf)){
+    if (JSON.stringify(firstHalf) === JSON.stringify(secondHalf)) {
         return true;
     }
     return false;
-    
+
 }
 
 palindrome("0_0 (: /-\ :) 0-0")
+// simplified version:
+function palindrome(str) {
+    // Remove non-alphanumeric characters and convert to lowercase
+    const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+    // Check if the cleaned string is equal to its reverse
+    return cleanedStr === cleanedStr.split('').reverse().join('');
+}
+
+const result = palindrome("0_0 (: /-\ :) 0-0");
+console.log(result); // true
