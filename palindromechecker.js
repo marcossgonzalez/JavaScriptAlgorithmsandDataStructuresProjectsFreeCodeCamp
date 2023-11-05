3d3be81b6ef73f8c3b894662368770cc39a8b393
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-11-05 02:58:22 -0300

    Update challenge
    update ongoing challenge

--- palindromechecker.js
+++ palindromechecker.js
@@ -3,10 +3,25 @@ function palindrome(str) {
     let string = str;
     console.log(string);
     let regex = /[^\W][a-zA-Z]*/gi;
-    string = string.match(regex).join('').replace('_','');
-    for (let i = 0)
-    console.log(string);
+    string = string.match(regex).join('').replace('_','').toLowerCase();
+    let stringLengthEven = (string.length/2) % 2;
+    let stringLength = string.length;
+    console.log(stringLength);
+    if(stringLength % 2 != 0){
+        let letterDoubleIndex = Math.floor(stringLength/2);
+        string = string.split('');
+        console.log(string);
+        string.splice(letterDoubleIndex, 0, string[letterDoubleIndex]);
+        console.log(string);
+    }
+    stringLength = Math.ceil(string.length/2);
+    console.log(stringLength)
+    let firstHalf = string.splice(0, stringLength);
+    let secondHalf = (string.splice(-stringLength)).reverse();
+    console.log(firstHalf)
+    console.log(secondHalf)
+    
     return true;
 }
 
-palindrome("0_0 (: /-\ :) 0-0")
+palindrome("eye")
