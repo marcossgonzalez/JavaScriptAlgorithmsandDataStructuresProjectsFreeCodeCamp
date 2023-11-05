b22632fd95d797c0ed47dbe59dacafb42b61116b
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-11-05 03:17:39 -0300

    Challenge 1 complete
    Challenge 1 complete

--- palindromechecker.js
+++ palindromechecker.js
@@ -9,19 +9,23 @@ function palindrome(str) {
     console.log(stringLength);
     if(stringLength % 2 != 0){
         let letterDoubleIndex = Math.floor(stringLength/2);
-        string = string.split('');
+        string = Array.from(string);
         console.log(string);
         string.splice(letterDoubleIndex, 0, string[letterDoubleIndex]);
         console.log(string);
     }
+    string = Array.from(string);
     stringLength = Math.ceil(string.length/2);
     console.log(stringLength)
     let firstHalf = string.splice(0, stringLength);
     let secondHalf = (string.splice(-stringLength)).reverse();
     console.log(firstHalf)
     console.log(secondHalf)
+    if(JSON.stringify(firstHalf) === JSON.stringify(secondHalf)){
+        return true;
+    }
+    return false;
     
-    return true;
 }
 
-palindrome("eye")
+palindrome("0_0 (: /-\ :) 0-0")
