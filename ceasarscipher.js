ca4446121c81a23ac859aee84c98ce86e16eee3b
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-11-07 00:08:20 -0300

    Finished challende 3
    finished challenge 3

--- ceasarscipher.js
+++ ceasarscipher.js
@@ -1,6 +1,30 @@
 //--------------------------------------------
 function rot13(str) {
-    return str;
+    let alphabet = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
+    let regex = /[a-zA-Z]/;
+    let arrayRot = str;
+    let count = 0;
+    let stringFinal = '';
+
+    while (count < arrayRot.length) {
+        if (arrayRot[count].match(regex)) {
+            stringFinal += alphabet[arrayRot[count].replace(arrayRot[count], (alphabet.indexOf(arrayRot[count])) + 13)]
+            count++;
+        } else {
+            stringFinal += arrayRot[count]
+            count++;
+        }
+    } return stringFinal
 }
 
 rot13("SERR PBQR PNZC");
+
+//simplier version
+function rot13(str) {
+return str.replace(/[A-Z]/g, (char) => {
+    const charCode = char.charCodeAt(0);
+    const base = char >= 'A' && char <= 'Z' ? 'A'.charCodeAt(0) : 'a'.charCodeAt(0);
+    const rotatedChar = String.fromCharCode((charCode - base + 13) % 26 + base);
+    return rotatedChar;
+  });
+}
