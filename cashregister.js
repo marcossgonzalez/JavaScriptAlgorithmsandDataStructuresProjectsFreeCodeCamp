b98a28cd6302c6ea45cc666fcc76ad504b0f76fd
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-11-07 19:23:44 -0300

    Update challenge 5
    ongoing challenge 5

--- cashregister.js
+++ cashregister.js
@@ -13,8 +13,7 @@ function checkCashRegister(price, cash, cid) {
         for (let i = 0; i<arrayCashValues.length; i++){
             if(change/arrayCashValues[i][1] >= 1 && i>0){     
                     countNow = change/arrayCashValues[i][1];
-                    if (countNow == change){
-                    valueReturn = (arrayCashValues[i][1]*countNow);}
+                    valueReturn = (arrayCashValues[i][1]*countNow);
                     console.log(valueReturn)
                     arrayCashInDrawer[i][1] -= valueReturn
                     returnsIndex = i
@@ -26,4 +25,4 @@ function checkCashRegister(price, cash, cid) {
     }console.log(arrayReturns)
     return change;
 }
-checkCashRegister(19.4, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+checkCashRegister(19.7, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
